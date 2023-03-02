12456ac09ccd550c34749d6cd79c66eda6c581bf
hobart <kolusai@hotmail.com>
2023-03-02 17:31:47 +0800

    替换工作台快捷入口图标（图片待替换）

--- src/views/pc/dashboard/index.vue
+++ src/views/pc/dashboard/index.vue
@@ -4,8 +4,8 @@ import { useRouter } from "vue-router";
 import dataFilling from "../../../assets/icon/dashboard_dataFilling.png";
 import IssueWork from "../../../assets/icon/dashboard_issueWork.png";
 import dataAudit from "../../../assets/icon/dashborad_dataReview.png";
-import workIssueOverview from "../../../assets/icon/dashborad_dataReview.png";
-import fillingProcess from "../../../assets/icon/dashborad_dataReview.png";
+import workIssueOverview from "../../../assets/icon/dashborad_workIssueOverview.png";
+import fillingProcess from "../../../assets/icon/dashborad_fillingProcess.png";
 
 export default defineComponent({
   setup() {
@@ -36,7 +36,7 @@ export default defineComponent({
       },
       {
         name: "填报流程",
-        icon: dataFilling,
+        icon: fillingProcess,
         path: "fillingProcess",
       },
     ];
