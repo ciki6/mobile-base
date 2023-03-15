385a35fea378a988c11fb2a132a0d470b83dc385
ciki6 <18694942447@163.com>
2023-03-15 17:13:30 +0800

    添加base

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import AutoImport from "unplugin-auto-import/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import { VantResolver } from "unplugin-vue-components/resolvers";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
+import path from 'path';
 
 const resolve = (dir) => path.resolve(__dirname, dir);
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobile',
   plugins: [
     vue(),
     AutoImport({
@@ -20,8 +21,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": resolve("src"), //作为 entries 的选项
+      '@': resolve('src'), //作为 entries 的选项
     },
-    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
 });
